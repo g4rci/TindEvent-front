a167d9eb3ced5744816ccc8abf676b5c6e09f3a0
Miguel Dominguez Chamorro <mdchamorro7@gmail.com>
2020-04-13 18:00:52 +0200

    widget

--- my-app/src/components/SearchEvents.js
+++ my-app/src/components/SearchEvents.js
@@ -6,7 +6,7 @@ import css from "../index.css"
 class SearchEvents extends React.Component {
     componentDidMount() {
         const script = document.createElement("script");
-        script.src = "https://ticketmaster-api-staging.github.io/products-and-docs/widgets/event-discovery/1.0.0/lib/main-widget.js";
+        script.src = "https://ticketmaster-api-staging.github.io/products-and-docs/widgets/calendar/1.0.0/lib/main-widget.js";
         script.async = true;
         script.defer = true;
         document.body.appendChild(script);
@@ -19,8 +19,9 @@ class SearchEvents extends React.Component {
     }
     render() {
         return (
-        <div id="mycustom-container" w-type="event-discovery" w-tmapikey="QDt0kSZEGRnYXjA1swZt9oSdGKMnjQM1" w-googleapikey="AIzaSyDhVx0oq3L3to4xqkVvu3pv9wtY5pTCuUw" w-keyword="" w-theme="simple" w-colorscheme="light" w-width="350" w-height="600" w-size="25" w-border="0" w-borderradius="4" w-postalcode="" w-radius="25" w-city="barcelona" w-period="year" w-layout="vertical" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="xxl" w-titlelink="off" w-sorting="groupByName" w-id="id_e7qqga" w-countrycode="ES" w-source="" w-branding="Ticketmaster" w-latlong=""></div>
-         ) }
+    <div w-type="calendar" w-tmapikey="QDt0kSZEGRnYXjA1swZt9oSdGKMnjQM1" w-googleapikey="AIzaSyDhVx0oq3L3to4xqkVvu3pv9wtY5pTCuUw" w-keyword="" w-theme="calendar" w-colorscheme="dark" w-width="298" w-height="400" w-size="25" w-border="1" w-borderradius="15" w-postalcode="" w-radius="25" w-countrycode="ES" w-city="barcelona" w-period="week" w-periodweek="week" w-layout="vertical" w-classificationid="" w-attractionid="" w-promoterid="" w-venueid="" w-affiliateid="" w-segmentid="" w-proportion="standart" w-latlong=""></div>        
+    
+    ) }
 }
 
-export default SearchEvents
+export default SearchEvents;
